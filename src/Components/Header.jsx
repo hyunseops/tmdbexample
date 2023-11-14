0b2e84be0dcc208a7d73ae6cd3d44f9ceda7f8ac
hyunseops <dkrmrmd122@naver.com>
2023-11-15 02:12:01 +0900

    header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,13 @@
 import './Header.css'
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 const Header = () => {
 
   return (
     <div className="header">
+      <Button variant="text">Text</Button>
 
     </div>
   )
